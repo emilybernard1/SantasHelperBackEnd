fb68dd53347a50e0261ae795c4387fc207048fe8
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-12-09 15:00:27 -0600

    changed the capitalization on line 5 of dbjs

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 
 // creating a base name for the mongodb
 // REPLACE THE STRING WITH YOUR OWN DATABASE NAME
-const mongooseBaseName = 'SantasHelperBackEnd'
+const mongooseBaseName = 'santashelperbackend'
 
 // create the mongodb uri for development and test
 const database = {
